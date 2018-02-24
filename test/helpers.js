41595e5fdaa10819ec9767df443c098713673181
Oleg Gromov <mail@oleggromov.com>
2018-02-24 15:49:42 +0100

    Updates tests for SauronStyle

--- test/helpers.js
+++ test/helpers.js
@@ -1,2 +1,14 @@
 export const MutationObserverMock = () => ({ observe: () => {} })
-export const getComputedStyleMock = () => {}
+export const getComputedStyleMock = () => {
+  let style = {}
+  return {
+    fn: () => style,
+    update: newStyle => {
+      for (let key in newStyle) {
+        if (newStyle.hasOwnProperty(key)) {
+          style[key] = newStyle[key]
+        }
+      }
+    }
+  }
+}
--- test/sauron-style.test.js
+++ test/sauron-style.test.js
@@ -2,17 +2,33 @@ import SauronStyle from '../src/sauron-style'
 import { MutationObserverMock, getComputedStyleMock } from './helpers'
 
 describe('SauronStyle', () => {
+  const getComputedStyle = getComputedStyleMock()
+  global.window.getComputedStyle = getComputedStyle.fn
   global.window.MutationObserver = MutationObserverMock
-  global.window.getComputedStyle = getComputedStyleMock
 
-  let node, styleObserver
+  let node, styleObserver, listener
 
   beforeEach(() => {
     node = document.documentElement.appendChild(document.createElement('div'))
     styleObserver = new SauronStyle(node)
+    listener = jest.fn()
+    styleObserver.subscribe(listener)
   })
 
-  it('test', () => {
-    expect(typeof styleObserver).toBe('object')
+  it('checkDiff without diff doesn\'t call the listener', () => {
+    styleObserver.checkDiff()
+    expect(listener.mock.calls.length).toBe(0)
+  })
+
+  it('checkDiff with diff invokes the listener and passes the right diff', () => {
+    getComputedStyle.update({ 'font-family': 'Times New Roman' })
+    styleObserver.checkDiff()
+    expect(listener.mock.calls.length).toBe(1)
+    expect(listener.mock.calls[0][0]).toEqual({
+      'font-family': {
+        cur: 'Times New Roman',
+        prev: undefined
+      }
+    })
   })
 })
