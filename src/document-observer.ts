0aae286d6f21e01e91d5ab63f9371b6841be1652
Oleg Gromov <mail@oleggromov.com>
2018-02-25 23:01:58 +0100

    Translates DocumentObserver

--- src/document-observer.ts
+++ src/document-observer.ts
@@ -1,12 +1,27 @@
-const isLink = node => node.tagName === 'LINK' && node.rel === 'stylesheet'
-const isStyle = node => node.tagName === 'STYLE'
-const getArray = list => Array.prototype.filter.call(list, node => isStyle(node) || isLink(node))
+const isLink = (node: HTMLLinkElement): boolean =>
+  node.tagName === 'LINK' && node.rel === 'stylesheet'
+
+const isStyle = (node: HTMLElement): boolean =>
+  node.tagName === 'STYLE'
+
+const getArray = (list: NodeList): Array<HTMLElement> =>
+  Array.prototype.filter.call(list, (node: Node): boolean =>
+    isStyle(<HTMLElement>node) || isLink(<HTMLLinkElement>node)
+  )
 
 class DocumentObserver {
+  private nextId: number
+  private observer: MutationObserver
+  private listeners: {
+    [key: number]: Function
+  }
+
   constructor () {
     this.nextId = 0
     this.listeners = {}
-    this.observer = new window.MutationObserver(mutations => mutations.forEach(this.observe.bind(this)))
+    this.observer = new (<any>window).MutationObserver((mutations: Array<MutationRecord>) =>
+      mutations.forEach(this.observe.bind(this))
+    )
     this.observer.observe(window.document, {
       attributes: true,
       attributeFilter: ['class'],
@@ -15,16 +30,16 @@ class DocumentObserver {
     })
   }
 
-  addListener (listener) {
+  addListener (listener: Function): number {
     this.listeners[this.nextId] = listener
     return this.nextId++
   }
 
-  removeListener (id) {
+  removeListener (id: number): void {
     delete this.listeners[id]
   }
 
-  invokeAll () {
+  invokeAll (): void {
     for (let key in this.listeners) {
       if (this.listeners.hasOwnProperty(key)) {
         this.listeners[key]()
@@ -32,7 +47,7 @@ class DocumentObserver {
     }
   }
 
-  observe (mutation) {
+  observe (mutation: MutationRecord): void {
     if (mutation.type === 'childList') {
       this.checkElements(mutation)
     } else if (mutation.type === 'attributes') {
@@ -40,17 +55,17 @@ class DocumentObserver {
     }
   }
 
-  checkElements (mutation) {
+  checkElements (mutation: MutationRecord): void {
     const added = getArray(mutation.addedNodes)
     const removed = getArray(mutation.removedNodes)
 
     if (added.length) {
-      added.forEach(node => {
-        if (isLink(node)) {
+      added.forEach((node: Node) => {
+        if (isLink(<HTMLLinkElement>node)) {
           node.addEventListener('load', () => this.invokeAll())
         }
 
-        if (isStyle(node)) {
+        if (isStyle(<HTMLElement>node)) {
           this.invokeAll()
         }
       })
@@ -62,8 +77,8 @@ class DocumentObserver {
   }
 }
 
-let instance
-const getDocumentObserver = () => {
+let instance: DocumentObserver
+const getDocumentObserver = (): DocumentObserver => {
   if (!instance) {
     instance = new DocumentObserver()
   }
